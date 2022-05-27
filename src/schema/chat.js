d1196e0bcc214e3c6254141123bc49eddce3fea8
Kamil Żak <kamil.zak.pl@outlook.com>
2022-05-27 16:48:33 +0200

    Add update/delete chat mutations

--- src/schema/chat.js
+++ src/schema/chat.js
@@ -5,14 +5,31 @@ import verifyAuthData from '../helpers/verifyAuthData.js'
 export const chatTypes = gql`
     type Query {
         getChats(userId: ID!): [Chat!]! @auth
+        getChat(chatId: ID!): Chat!
     }
     type Mutation {
-        createChat(userId: ID!, name: String!): Chat @auth
+        createChat(userId: ID!, details: ChatInput!): Chat! @auth
+        updateChat(chatId: ID!, details: ChatInput!): Chat! @auth
+        deleteChat(chatId: ID!): DeletedChat @auth
     }
 
     type Chat {
         id: ID!
         name: String!
+        title: String!
+        subtitle: String!
+        color: String!
+    }
+
+    type DeletedChat {
+        id: ID!
+    }
+
+    input ChatInput {
+        name: String!
+        title: String!
+        subtitle: String!
+        color: String!
     }
 `
 export const chatResolvers = {
@@ -22,13 +39,33 @@ export const chatResolvers = {
             const chats = await Chat.query().where({ userId })
             return chats
         },
+        getChat: async (_, { chatId }) => {
+            const chat = await Chat.query().findById(chatId)
+            return chat
+        },
     },
     Mutation: {
-        createChat: async (_, { userId, name }, { authData }) => {
+        createChat: async (_, { userId, details }, { authData }) => {
             verifyAuthData(authData, { userId })
 
-            const { id } = await Chat.query().insert({ userId, name })
-            return { id, name }
+            const { id } = await Chat.query().insert({ userId, ...details })
+            return { ...details, id }
+        },
+        updateChat: async (_, { chatId, details }, { authData }) => {
+            const { userId } = await Chat.query().findById(chatId).select('userId')
+            verifyAuthData(authData, { userId })
+
+            await Chat.query()
+                .findById(chatId)
+                .update({ ...details })
+
+            return { ...details, id: chatId }
+        },
+        deleteChat: async (_, { chatId }, { authData }) => {
+            const { userId } = await Chat.query().findById(chatId).select('userId')
+            verifyAuthData(authData, { userId })
+            await Chat.query().deleteById(chatId)
+            return { id: chatId }
         },
     },
 }
